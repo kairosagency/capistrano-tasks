c92c19548f16659062aaae60f4fbee0733b75106
Alexandre Assouad <alexandre.assouad@gmail.com>
2015-09-28 14:35:59 +0000

    tasks should work

--- lib/capistrano/tasks.rb
+++ lib/capistrano/tasks.rb
@@ -2,8 +2,8 @@ require 'capistrano/framework'
 require 'capistrano/file-permissions'
 
 # load all tasks
-Dir.glob('./tasks/*.rake').each { |task| load File.expand_path(task)}
-# load File.expand_path('../tasks/composer.rake', __FILE__)
-# load File.expand_path('../tasks/phpfpm.rake', __FILE__)
-# load File.expand_path('../tasks/react.rake', __FILE__)
-# load File.expand_path('../tasks/permissions.rake', __FILE__)
+# Dir.glob('./tasks/*.rake').each { |task| load File.expand_path(task)}
+load File.expand_path('../tasks/composer.rake', __FILE__)
+load File.expand_path('../tasks/phpfpm.rake', __FILE__)
+load File.expand_path('../tasks/react.rake', __FILE__)
+load File.expand_path('../tasks/permissions.rake', __FILE__)
